4231f90bdae05379d72b82a4073c05d852509643
LCHansson <love.hansson@gmail.com>
2014-09-28 23:30:54 +0200

    Initial commit

---
+++ ui.R
@@ -0,0 +1,37 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+  includeCSS("www/lchansson.css"),
+  
+  tags$head(
+    HTML("<link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>")
+  ),
+  
+  h1('Hej'),
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
