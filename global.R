01219eae3003c173537fe5d4fdbb06bcf556d20f
LCHansson <love.hansson@gmail.com>
2014-10-12 14:29:48 +0200

    Clean up startup script

--- global.R
+++ global.R
@@ -3,27 +3,29 @@
 # All code put in here is run once, at startup.
 
 
-# Website variables
-web_title = 'Utvecklingsrådet'
-footer_text = "Generated with <a href='https://github.com/lchansson/braceana'>Braceana</a>, a markdown-based CMS written by <a href='https://github.com/lchansson'>Love Hansson</a>, 2014. Built using <a href='http://shiny.rstudio.com'>Shiny</a>. Framework licensed under the <a href='http://www.gnu.org/licenses/agpl-3.0.html'>AGPL v3</a>."
+## Website variables ----
+# These are content parameters for your website.
+web_title = 'Braceana'
+footer_text = "Generated with <a href='https://github.com/lchansson/braceana'>Braceana</a>, a markdown-based CMS written by <a href='https://github.com/lchansson'>LCHansson</a>, 2014. Built using <a href='http://shiny.rstudio.com'>Shiny</a>. Framework licensed under the <a href='http://www.gnu.org/licenses/agpl-3.0.html'>AGPL v3</a>."
 blog_name = 'Blogg'
 
 blog_path = 'blog'
 pages_path = 'pages'
 
-# Run options
-options("shiny.launch.browser" = TRUE)
-options(shiny.reactlog = TRUE)
 
-# Source functions
-sourceFiles <- list.files(file.path(getwd(), 'app/functions'), full.names = TRUE)
-for (sourceFile in sourceFiles)
+##
+## Don't change any code below this line if you don't know what you're doing. ##
+##
+
+## Set run options ----
+options("shiny.launch.browser" = TRUE)
+options(shiny.reactlog = FALSE)
+
+## Include widgets and custom libraries ----
+files <- c('app/functions', 'app/widgets')
+
+for (sourceFile in list.files(file.path(getwd(), files), full.names = TRUE)
      source(sourceFile)
 
-widgets <- list.files(file.path(getwd(), 'app/widgets'), full.names = TRUE)
-for (widget in widgets)
-  source(widget)
-
-
-# Functions to run
+## Functions to run at startup ----
 knitAllRmd(folder = c(blog_path, pages_path), recompile = TRUE)
